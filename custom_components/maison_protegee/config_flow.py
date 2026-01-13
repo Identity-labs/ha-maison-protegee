fc06dadf05d867d053f9ac53ed5adfcac97ee94c
Identity-labs <600296+Identity-labs@users.noreply.github.com>
2026-01-13 17:42:48 +0100

    fix: edit avoid reauth if no change

--- custom_components/maison_protegee/config_flow.py
+++ custom_components/maison_protegee/config_flow.py
@@ -102,39 +102,51 @@ class MaisonProtegeeOptionsFlowHandler(config_entries.OptionsFlow):
         config_entry = self.config_entry
 
         if user_input is not None:
-            password = user_input.get(CONF_PASSWORD, "").strip()
-            if not password:
-                password = config_entry.data.get(CONF_PASSWORD)
+            new_password = user_input.get(CONF_PASSWORD, "").strip()
+            current_password = config_entry.data.get(CONF_PASSWORD)
+            current_username = config_entry.data.get(CONF_USERNAME)
             
-            if not password:
-                errors["base"] = "password_required"
-            else:
-                session = aiohttp.ClientSession()
-                try:
-                    api = MaisonProtegeeAPI(
-                        user_input[CONF_USERNAME],
-                        password,
-                        session,
-                    )
+            password_changed = bool(new_password) and new_password != current_password
+            username_changed = user_input[CONF_USERNAME] != current_username
+            
+            if password_changed or username_changed:
+                password_to_use = new_password if password_changed else current_password
+                username_to_use = user_input[CONF_USERNAME]
+                
+                if not password_to_use:
+                    errors["base"] = "password_required"
+                else:
+                    session = aiohttp.ClientSession()
+                    try:
+                        api = MaisonProtegeeAPI(
+                            username_to_use,
+                            password_to_use,
+                            session,
+                        )
 
-                    auth_result = await api.async_authenticate()
-                    if auth_result:
+                        auth_result = await api.async_authenticate()
+                        if auth_result:
+                            await session.close()
+                            updated_data = dict(config_entry.data)
+                            updated_data[CONF_USERNAME] = username_to_use
+                            updated_data[CONF_PASSWORD] = password_to_use
+                            updated_data[CONF_ENABLE_TEMPERATURES] = user_input.get(CONF_ENABLE_TEMPERATURES, True)
+                            updated_data[CONF_ENABLE_EVENTS] = user_input.get(CONF_ENABLE_EVENTS, True)
+                            return self.async_create_entry(data=updated_data)
+                        errors["base"] = "invalid_auth"
+                    except aiohttp.ClientError as err:
+                        _LOGGER.exception("Connection error during authentication: %s", err)
+                        errors["base"] = "cannot_connect"
+                    except Exception as err:
+                        _LOGGER.exception("Unexpected exception during authentication: %s", err)
+                        errors["base"] = "unknown"
+                    finally:
                         await session.close()
-                        updated_data = dict(config_entry.data)
-                        updated_data[CONF_USERNAME] = user_input[CONF_USERNAME]
-                        updated_data[CONF_PASSWORD] = password
-                        updated_data[CONF_ENABLE_TEMPERATURES] = user_input.get(CONF_ENABLE_TEMPERATURES, True)
-                        updated_data[CONF_ENABLE_EVENTS] = user_input.get(CONF_ENABLE_EVENTS, True)
-                        return self.async_create_entry(data=updated_data)
-                    errors["base"] = "invalid_auth"
-                except aiohttp.ClientError as err:
-                    _LOGGER.exception("Connection error during authentication: %s", err)
-                    errors["base"] = "cannot_connect"
-                except Exception as err:
-                    _LOGGER.exception("Unexpected exception during authentication: %s", err)
-                    errors["base"] = "unknown"
-                finally:
-                    await session.close()
+            else:
+                updated_data = dict(config_entry.data)
+                updated_data[CONF_ENABLE_TEMPERATURES] = user_input.get(CONF_ENABLE_TEMPERATURES, True)
+                updated_data[CONF_ENABLE_EVENTS] = user_input.get(CONF_ENABLE_EVENTS, True)
+                return self.async_create_entry(data=updated_data)
 
         return self.async_show_form(
             step_id="init",
